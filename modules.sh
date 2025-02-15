2c0f3ddbd8518c79fbdc615d4e739e27d0ca73f6
Mateus Almeida <mateusalmeida0070@gmail.com>
2025-02-15 18:57:57 -0300

    chore: Adjusting the module.sh

--- modules.sh
+++ modules.sh
@@ -1,10 +1,16 @@
+# Core dependencies
 deno add jsr:@std/dotenv
-deno add jsr:@hono/hono
 deno add jsr:@std/assert
 
-deno add npm:drizzle-orm 
+# HTTP and validation
+deno add npm:hono
+deno add npm:@hono/zod-validator
+deno add npm:zod
+deno add npm:hono-openapi
+deno add npm:zod-openapi
+
+# Database
+deno add npm:drizzle-orm
 deno add npm:drizzle-kit
-
-deno add npm:mysql2 
-
+deno add npm:mysql2
 
